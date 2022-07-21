d2987cebe694555c1d8b873ad9630b026704c066
Fachru Dahri <fachru.dahri@gmail.com>
2022-07-21 22:13:58 +0700

    Initial Commit

---
+++ CloneApp/CloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneAppApp.swift
+//  CloneApp
+//
+//  Created by fachru dahri on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
